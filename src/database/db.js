744d681dc0adb3c2eca3fe38f44ea7fde9f8e480
dicolab5 <dico.colab5@gmail.com>
2025-11-24 10:09:49 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,8 @@
-//para usar localmente
+// src/db.js para usar no render.com
 const knex = require('knex');
 require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const db = knex({
   client: 'pg',
@@ -9,7 +11,10 @@ const db = knex({
     port: process.env.PG_PORT,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE
+    database: process.env.PG_DATABASE,
+    ssl: isProduction
+      ? { rejectUnauthorized: false }  // Render/Cloud geralmente precisam disso
+      : false
   },
   pool: {
     min: 0,
@@ -18,3 +23,25 @@ const db = knex({
 });
 
 module.exports = db;
+
+// //para usar localmente
+// const knex = require('knex');
+// require('dotenv').config();
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     user: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     database: process.env.PG_DATABASE
+//   },
+//   pool: {
+//     min: 0,
+//     max: 10
+//   }
+// });
+
+
+// module.exports = db;
