039f36cae01ae84ab79d91ff2bb1b56a417c940b
dicolab5 <dico.colab5@gmail.com>
2025-11-25 18:04:39 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,29 +1,6 @@
-//para usar localmente
-const knex = require('knex');
-require('dotenv').config();
-
-const db = knex({
-  client: 'pg',
-  connection: {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE
-  },
-  pool: {
-    min: 0,
-    max: 10
-  }
-});
-
-module.exports = db;
-
-// // src/db.js para usar no render.com
+// //para usar localmente
 // const knex = require('knex');
 // require('dotenv').config();
-
-// const isProduction = process.env.NODE_ENV === 'production';
 
 // const db = knex({
 //   client: 'pg',
@@ -32,10 +9,7 @@ module.exports = db;
 //     port: process.env.PG_PORT,
 //     user: process.env.PG_USER,
 //     password: process.env.PG_PASSWORD,
-//     database: process.env.PG_DATABASE,
-//     ssl: isProduction
-//       ? { rejectUnauthorized: false }  // Render/Cloud geralmente precisam disso
-//       : false
+//     database: process.env.PG_DATABASE
 //   },
 //   pool: {
 //     min: 0,
@@ -44,3 +18,29 @@ module.exports = db;
 // });
 
 // module.exports = db;
+
+// src/db.js para usar no render.com
+const knex = require('knex');
+require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: isProduction
+      ? { rejectUnauthorized: false }  // Render/Cloud geralmente precisam disso
+      : false
+  },
+  pool: {
+    min: 0,
+    max: 10
+  }
+});
+
+module.exports = db;
