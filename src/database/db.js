07e1337c109373ea084e7d23435931463a33dbcb
dicolab5 <dico.colab5@gmail.com>
2025-11-26 16:40:05 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,24 +1,3 @@
-// //para usar localmente
-// const knex = require('knex');
-// require('dotenv').config();
-
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     user: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD,
-//     database: process.env.PG_DATABASE
-//   },
-//   pool: {
-//     min: 0,
-//     max: 10
-//   }
-// });
-
-// module.exports = db;
-
 // src/db.js para usar no render.com
 const knex = require('knex');
 require('dotenv').config();
@@ -44,3 +23,4 @@ const db = knex({
 });
 
 module.exports = db;
+
